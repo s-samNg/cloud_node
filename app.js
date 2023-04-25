ffd1ad961e30dddad2f29b998bc7c9949c15057e
issam <issam.neggaoui@carbase.fr>
2023-04-25 15:40:21 +0200

    commit provisoire

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const app = express()
 
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })) 
 app.use("/api", router);
 
-app.use(express.json());
 
 
 
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,7 +1,35 @@
-exports.signup = (req, res) => {
-    res.send('You are signup'); 
+// const User = require('../models/user');
+
+exports.signup = async (req, res) => {
+    try{
+        const user = await User.create({ 
+            firstName: "John",
+            lastName: "Doe",
+            email: "john.doe@mail.com",
+            password: "123456"
+          });
+
+          res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+
+    }
+   
+
+    // console.log(req.body)
+    // res.json(req.body)
+    // res.send('You are signup'); 
 }
 
-exports.login = (req, res) => {
-    res.send('You are login'); 
-}
+
+
+// exports.login = (req, res) => {
+//     console.log(req.body)
+//     res.json(req.body)
+//     res.send('You are login'); 
+// }
+
+
+
+
+  
