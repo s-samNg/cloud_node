5b8550288f0e9ad5e26ed9ee5943e5e24377f3b3
issam <issam.neggaoui@carbase.fr>
2023-04-26 13:14:15 +0200

    commit prov

--- app/controllers/woods.js
+++ app/controllers/woods.js
@@ -1,3 +1,16 @@
-exports.readALL = (req, res) => {
-    res.send('List of woods'); 
+const { Wood } = require('../models');
+
+exports.readALL = async (req, res) => {
+
+    try {
+        const woods = await Wood.findAll();
+
+        res.status(201).json({ woods });
+
+    } catch (error) {
+        
+        console.log(error);
+        res.status(500).json({ error: 'Erreur, contactez le support' });
+    }
+    
 }
