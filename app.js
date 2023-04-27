3ff1275f312c6c2f24ca9486089bd3551b34bf41
issam <issam.neggaoui@carbase.fr>
2023-04-27 15:26:08 +0200

    commit prov

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = require("./app/routes/index.js");
 const db = require("./app/models/index.js"); 
+var path = require('path');
 const app = express() 
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/api", router);
 
 
