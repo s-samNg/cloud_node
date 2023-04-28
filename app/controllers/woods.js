b4b578e6ab2155c35fd359d2b65601d3dd85dcdb
issam <issam.neggaoui@carbase.fr>
2023-04-28 10:29:04 +0200

    commit prov

--- app/controllers/woods.js
+++ app/controllers/woods.js
@@ -39,15 +39,25 @@ exports.create = async (req, res) => {
 
     try {
 
-        const pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+        const pathname = null; 
 
+        if (req.file) {
+            
+           pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+
+        }else {
+            res.status(400).json({ error: "Uploadez votre image" });
+        }
+        
         const wood = await Wood.create({ ...JSON.parse(req.body.datas), image: pathname });
         res.status(201).json(wood);
+        
+        
 
     } catch (error) {
 
         console.error(error);
-        res.status(500).json({ message: 'Erreur, contactez le support' });
+        res.status(500).json({ error: 'Erreur, contactez le support' });
 
     }
     
